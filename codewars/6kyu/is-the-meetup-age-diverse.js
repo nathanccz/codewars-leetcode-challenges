073e5174d2679ff47d17c055723876124aa353eb
Nathan Casarez <n.casarez@icloud.com>
2024-09-06 12:33:38 -0700

    Added Codewars level 6kyu challenge and solution for 09-06-24.

---
+++ codewars/6kyu/is-the-meetup-age-diverse.js
@@ -0,0 +1,89 @@
+/*
+Date of completion: 09-06-2024 
+Codewars Level 6 kyu
+
+--------------------------------------------------------
+CHALLENGE DESCRIPTION
+--------------------------------------------------------
+
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if developers from all of the following age groups have signed up: teens, twenties, thirties, forties, fifties, sixties, seventies, eighties, nineties, centenarian (at least 100 years young).
+false otherwise.
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+  { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+  { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+  { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript' }
+];
+
+your function should return true as there is at least one developer from each age group.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+Age is represented by a number which can be any positive integer up to 199.
+
+
+--------------------------------------------------------
+ANALYSIS
+-------------------------------------------------------- 
+
+Parameters:
+
+An array of objects representing users
+
+-----
+
+Return:
+
+A Boolean
+
+-----
+
+Example: 
+
+See description above.
+
+------
+
+Pseudo Code: 
+
+function isAgeDiverse(list)
+    const ageGroups = []
+
+    for each dev in list
+        let ageGroup = Math.floor(dev age / 10)
+        if (ageGroup > 10) ageGroup = 10
+        if (ageGroups doesnt include ageGroup) push to ageGroups
+    
+    return ageGroups.length is equal to 10
+
+--------------------------------------------------------
+SOLUTION
+-------------------------------------------------------- */
+
+function isAgeDiverse(list) {
+    if (list.length < 10) return false
+  
+    const ageGroups = []
+
+    for (let dev of list) {
+        let curr = Math.floor(dev.age / 10) * 10
+        if (curr > 100) curr = 100
+        if (!ageGroups.includes(curr) && curr > 0) ageGroups.push(curr)
+    }
+  
+    return ageGroups.length === 10
+}
