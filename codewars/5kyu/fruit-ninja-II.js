28ffbc5875f4cf1e6e23a53f9ea6fbc202e98b61
Nathan Casarez <nathanweb.dev@proton.me>
2025-10-01 12:41:42 -0700

    Added Codewars challenge and solution for 10-01-25.

---
+++ codewars/5kyu/fruit-ninja-II.js
@@ -0,0 +1,116 @@
+/*
+Date of completion: 09-26-2025 
+Codewars Level 5 kyu
+
+--------------------------------------------------------
+CHALLENGE DESCRIPTION
+--------------------------------------------------------
+
+https://www.codewars.com/kata/57d60e5476da831f5600011e/train/javascript
+
+Description:
+You are a fruit ninja, your skill is cut fruit. You cut some fruit by your knife in last kata. You want to know how many fruits have been successfully cut, so you need to count the number of fruits. For example:
+
+halfFruits=["app","le","pe","ar","ban","ana"]
+countFruits(halfFruits)={"apple":1,"pear":1,"banana":1}
+
+As you see, you should return an object that contains all fruits and its number. We only count the whole fruit, the excess will be ignored:
+
+halfFruits=["app","le","app","le","le","le"]
+countFruits(halfFruits)={"apple":2}  //two "le" has been ignored
+
+The fruit which are not uniformly cut will be ignored. That is, When the length of fruit's name is a even number, the length of two parts should equal; When the length of fruit's name is an odd number, the left part is 1 character longer than right part.
+
+halfFruits=["app","le","appl","e","ap","ple","ale","pp","bo","mb"]
+countFruits(halfFruits)={"apple":1}  //only "app","le" is what we want
+
+In order to make clear what is the fruit, you may need a list fruitsName. It's a preloaded variable, you can use it directly. e` :[
+
+Task:
+Complete function countFruits that accepts an argument halfFruits. Returns the result in accordance with the rules above.
+
+--------------------------------------------------------
+ANALYSIS
+-------------------------------------------------------- 
+
+Parameters:
+
+An array of strings
+
+-----
+
+Return:
+
+An object with string keys and number values
+
+-----
+
+Examples: 
+
+countFruits(["app","le","pe","ar","ban","ana"]) returns {"apple":1,"pear":1,"banana":1}
+
+countFruits(["app","le","app","le","le","le"]) returns {"apple":2}
+
+countFruits(["app","le","appl","e","ap","ple"]) returns {"apple":1}
+
+countFruits(["le","le","app","app","app"]) returns {"apple":2}
+
+------
+
+Pseudo Code: 
+
+function countFruits(halfFruits)
+    create result variable, initialized as empty object
+    
+    loop through halfFruits
+        if 
+--------------------------------------------------------
+SOLUTION
+-------------------------------------------------------- */
+
+const fruitsName = ['apple', 'pear', 'banana']
+
+function countFruits(halfFruits) {
+  const result = {}
+  const foundWords = {}
+
+  for (const half of halfFruits) {
+    for (const fruit of fruitsName) {
+      if (!foundWords[fruit]) {
+        foundWords[fruit] = {}
+        foundWords[fruit].firstHalf = []
+        foundWords[fruit].secondHalf = []
+      }
+
+      let length1, length2
+
+      if (fruit.length % 2 === 0) {
+        length1 = fruit.length / 2
+        length2 = fruit.length / 2
+      } else {
+        length1 = Math.ceil(fruit.length / 2)
+        length2 = Math.floor(fruit.length / 2)
+      }
+
+      if (fruit.startsWith(half) && half.length === length1) {
+        foundWords[fruit].firstHalf.push(half)
+      } else if (fruit.endsWith(half) && half.length === length2) {
+        foundWords[fruit].secondHalf.push(half)
+      }
+    }
+  }
+
+  for (const word in foundWords) {
+    const count = Math.min(
+      foundWords[word].firstHalf.length,
+      foundWords[word].secondHalf.length
+    )
+    if (count > 0) {
+      result[word] = count
+    }
+  }
+
+  return result
+}
+
+countFruits(['app', 'le', 'pe', 'ar', 'ban', 'ana'])
